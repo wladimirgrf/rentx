8f226808a29d6dc566670c05744ea4fb16339a40
Wladimir <wladimir.code@gmail.com>
2021-08-02 20:28:11 -0300

    feat: Eslint config

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from 'express'
+import express from "express";
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({msg: 'Ignite'});
+app.get("/", (request, response) => {
+  return response.json({ msg: "Ignite" });
 });
 
 app.listen(3333);
