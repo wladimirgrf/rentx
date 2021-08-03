0f6a8076188f0327c0b001532b0c2743ea1e5ef3
Wladimir <wladimir.code@gmail.com>
2021-08-03 20:08:18 -0300

    feat: Categories routes

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
 import express from "express";
 
+import { routes } from "./routes";
+
 const app = express();
 
 app.use(express.json());
-
-app.get("/", (request, response) => {
-  const { name } = request.query;
-
-  return response.json({ name });
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log("Server running");
