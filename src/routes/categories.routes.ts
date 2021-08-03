8b432079fc69967d812cd59994a13fe235f25015
Wladimir <wladimir.code@gmail.com>
2021-08-03 20:55:55 -0300

    feat: Categories repository

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,29 +1,20 @@
 import { Router } from "express";
 
-import { Category } from "../models/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
-const categories: Category[] = [];
-
-categoriesRoutes.get("/", (request, response) => {
-  return response.json(categories);
-});
+// categoriesRoutes.get("/", (request, response) => {
+//   return response.json(categories);
+// });
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
+  categoriesRepository.create({ name, description });
 
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
-
-  categories.push(category);
-
-  return response.json({ category });
+  return response.json();
 });
 
 export { categoriesRoutes };
