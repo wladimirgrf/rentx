e8ec0e086878d40c8bd092bc29b817d6f4e62fe3
Wladimir <wladimir.code@gmail.com>
2021-09-19 16:08:12 -0300

    feat: User password bcryptjs hash, email validation

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -26,6 +26,12 @@ class UserRepository implements IUsersRepository {
 
     await this.repository.save(user);
   }
+
+  async findByEmail(email: string): Promise<User> {
+    const user = await this.repository.findOne({ email });
+
+    return user;
+  }
 }
 
 export { UserRepository };
