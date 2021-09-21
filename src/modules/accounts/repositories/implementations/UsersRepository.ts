0909f8d5643a4345d7afa3310252550bfd76b459
Wladimir <wladimir.code@gmail.com>
2021-09-20 21:14:30 -0300

    feat: User repository, find by id

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -32,6 +32,12 @@ class UserRepository implements IUsersRepository {
 
     return user;
   }
+
+  async findById(id: string): Promise<User> {
+    const user = await this.repository.findOne(id);
+
+    return user;
+  }
 }
 
 export { UserRepository };
