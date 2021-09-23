ab05a720ba5d062f03beebf4eff765f59da4de35
Wladimir <wladimir.code@gmail.com>
2021-09-23 20:03:07 -0300

    feat: Error handler, AppError

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
 
+import "express-async-errors";
 import "./database";
 import "./shared/container";
 
+import { errorHandler } from "./middlewares/errorHandler";
 import { routes } from "./routes";
 import swaggerFile from "./swagger.json";
 
@@ -14,6 +16,8 @@ app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(routes);
+
+app.use(errorHandler);
 
 app.listen(3333, () => {
   console.log("Server running");
