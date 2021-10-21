25a645c9cc0cc8752f0121b4934605e21fbb5b61
Wladimir <wladimir.code@gmail.com>
2021-10-20 21:40:00 -0300

    feat: Cars route

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { authenticateRoutes } from "./authenticate.routes";
+import { carsRoutes } from "./cars.routes";
 import { categoriesRoutes } from "./categories.routes";
 import { specificationsRoutes } from "./specifications.routes";
 import { usersRoutes } from "./users.routes";
@@ -15,5 +16,6 @@ routes.use(ensureAuthenticated);
 routes.use("/categories", categoriesRoutes);
 routes.use("/specifications", specificationsRoutes);
 routes.use("/users", usersRoutes);
+routes.use("/cars", carsRoutes);
 
 export { routes };
