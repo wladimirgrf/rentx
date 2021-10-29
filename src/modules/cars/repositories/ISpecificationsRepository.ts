f199ebe6b8686c5c84702504afcd18e50916b3a0
wladimirgrf <wladimir.code@gmail.com>
2021-10-28 21:29:09 -0300

    feat: SpecificationsRepository, create return

--- src/modules/cars/repositories/ISpecificationsRepository.ts
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -7,7 +7,10 @@ interface ICreateSpecificationDTO {
 
 interface ISpecificationsRepository {
   list(): Promise<Specification[]>;
-  create({ name, description }: ICreateSpecificationDTO): Promise<void>;
+  create({
+    name,
+    description,
+  }: ICreateSpecificationDTO): Promise<Specification>;
   findByName(name: string): Promise<Specification>;
   findByIds(ids: string[]): Promise<Specification[]>;
 }
--- src/modules/cars/repositories/in-memory/SpecificationsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/SpecificationsRepositoryInMemory.ts
@@ -12,7 +12,10 @@ class SpecificationsRepositoryInMemory implements ISpecificationsRepository {
     return this.specifications;
   }
 
-  async create({ name, description }: ICreateSpecificationDTO): Promise<void> {
+  async create({
+    name,
+    description,
+  }: ICreateSpecificationDTO): Promise<Specification> {
     const specification = new Specification();
 
     Object.assign(specification, {
@@ -21,6 +24,8 @@ class SpecificationsRepositoryInMemory implements ISpecificationsRepository {
     });
 
     this.specifications.push(specification);
+
+    return specification;
   }
 
   async findByName(name: string): Promise<Specification> {
