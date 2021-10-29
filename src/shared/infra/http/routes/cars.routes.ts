361ec33a3aeab22349456327c583e87785863d15
wladimirgrf <wladimir.code@gmail.com>
2021-10-28 21:30:23 -0300

    feat: Create car specification, route and controller

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
+import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
 import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
 
 const carsAdminRoutes = Router();
@@ -8,8 +9,14 @@ const carsPublicRoutes = Router();
 
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
+const createCarSpecificationController = new CreateCarSpecificationController();
 
 carsPublicRoutes.get("/available", listAvailableCarsController.handle);
+
+carsAdminRoutes.post(
+  "/specifications/:id",
+  createCarSpecificationController.handle
+);
 
 carsAdminRoutes.post("/", createCarController.handle);
 
