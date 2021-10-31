4f7e3fc40435e37a75c4fda434a188c974c50c36
wladimirgrf <wladimir.code@gmail.com>
2021-10-31 16:25:44 -0300

    feat: Cars images controller, use case and route

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "@config/upload";
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
 import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
 import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
+import { UploadCarImagesController } from "@modules/cars/useCases/uploadCarImages/UploadCarImagesController";
 
 const carsAdminRoutes = Router();
 const carsPublicRoutes = Router();
@@ -10,12 +13,21 @@ const carsPublicRoutes = Router();
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
 const createCarSpecificationController = new CreateCarSpecificationController();
+const uploadCarImagesController = new UploadCarImagesController();
+
+const upload = multer(uploadConfig.upload("./tmp/cars"));
 
 carsPublicRoutes.get("/available", listAvailableCarsController.handle);
 
 carsAdminRoutes.post(
   "/specifications/:id",
   createCarSpecificationController.handle
+);
+
+carsAdminRoutes.post(
+  "/images/:id",
+  upload.array("images"),
+  uploadCarImagesController.handle
 );
 
 carsAdminRoutes.post("/", createCarController.handle);
