fc83721ea03585fe261b10ae19c18568929f5aa6
wladimirgrf <wladimir.code@gmail.com>
2021-11-13 14:59:52 -0300

    feat: Rental, typeorm entity

--- src/modules/rentals/infra/typeorm/entities/Rental.ts
+++ src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -1,28 +1,57 @@
+import {
+  Column,
+  Entity,
+  PrimaryColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 
+import { User } from "@modules/accounts/infra/typeorm/entities/User";
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
+
+@Entity("rentals")
 class Rental {
+  @PrimaryColumn()
   id: string;
 
-  car_id: string;
-
-  user_id: string;
-
+  @Column()
   start_date: Date;
 
+  @Column()
   end_date: Date;
 
+  @Column()
   expected_return_date: Date;
 
+  @Column()
   total: number;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
+
+  @Column()
+  car_id: string;
+
+  @ManyToOne(() => Car)
+  @JoinColumn({ name: "car_id" })
+  car: Car;
+
+  @Column()
+  user_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: "user_id" })
+  user: User;
 
   constructor() {
     if (!this.id) {
       this.id = uuidV4();
-      this.created_at = new Date();
     }
   }
 }
