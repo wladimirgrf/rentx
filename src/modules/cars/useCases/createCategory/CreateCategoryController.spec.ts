d9f84b24904a9d13c16752af4c5c9c301ef09382
wladimirgrf <wladimir.code@gmail.com>
2021-11-14 11:50:21 -0300

    feat: Create category controller test, supertest

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -0,0 +1,47 @@
+import request from "supertest";
+import { Connection } from "typeorm";
+
+import { app } from "@shared/infra/http/app";
+import createConnection from "@shared/infra/typeorm";
+import { createAdminUserQuery } from "@shared/infra/typeorm/seed/querys";
+
+let connection: Connection;
+
+describe("Create Category Controller", () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const query = await createAdminUserQuery();
+    await connection.query(query);
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it("should be able to create a new category", async () => {
+    const user = {
+      email: "admin@rentx.com",
+      password: "password123",
+    };
+    const responseToken = await request(app).post("/sessions").send(user);
+
+    const { token } = responseToken.body;
+
+    const category = {
+      name: "Category Supertest",
+      description: "Description category supertest",
+    };
+
+    const response = await request(app)
+      .post("/categories")
+      .send(category)
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    expect(response.status).toBe(201);
+  });
+});
