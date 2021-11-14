91cbbde1dd545e3d63f4ef9a87a203de0ab1276c
wladimirgrf <wladimir.code@gmail.com>
2021-11-14 09:07:19 -0300

    feat: export app structure, new server listen

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import swaggerUi from "swagger-ui-express";
+
+import "reflect-metadata";
+import "express-async-errors";
+import "@shared/container";
+
+import createConnection from "@shared/infra/typeorm";
+
+import swaggerFile from "../../../swagger.json";
+import { errorHandler } from "./middlewares/errorHandler";
+import { routes } from "./routes";
+
+createConnection();
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use(routes);
+
+app.use(errorHandler);
+
+export { app };
