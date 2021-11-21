b96b3c67080f1b2233de25ea51450b82a8ab5ec9
wladimirgrf <wladimir.code@gmail.com>
2021-11-21 14:03:25 -0300

    feat: List Rentals By User use case, controller and route

--- src/shared/infra/http/routes/rentals.routes.ts
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
+import { ListRentalsByUserController } from "@modules/rentals/useCases/listRentalsByUser/ListRentalsByUserController";
 import { ReturnRentalController } from "@modules/rentals/useCases/returnRental/ReturnRentalController";
 
 const rentalsRoutes = Router();
@@ -8,7 +9,11 @@ const rentalsRoutes = Router();
 const createRentalController = new CreateRentalController();
 const returnRentalController = new ReturnRentalController();
 
+const listRentalsByUserController = new ListRentalsByUserController();
+
 rentalsRoutes.post("/", createRentalController.handle);
 rentalsRoutes.put("/return/:id", returnRentalController.handle);
+
+rentalsRoutes.get("/user", listRentalsByUserController.handle);
 
 export { rentalsRoutes };
