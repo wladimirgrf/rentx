2619560091a0dc3e13783ceac15a4cf66a7b1f4d
wladimirgrf <wladimir.code@gmail.com>
2021-11-27 19:00:39 -0300

    feat: users tokens repository, findByUserIdAndRefreshToken

--- src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -3,20 +3,20 @@ import { getRepository, Repository } from "typeorm";
 import { ICreateUserTokenDTO } from "@modules/accounts/dtos/ICreateUserTokenDTO";
 import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
 
-import { UserTokens } from "../entities/UserTokens";
+import { UserToken } from "../entities/UserToken";
 
 class UsersTokensRepository implements IUsersTokensRepository {
-  private repository: Repository<UserTokens>;
+  private repository: Repository<UserToken>;
 
   constructor() {
-    this.repository = getRepository(UserTokens);
+    this.repository = getRepository(UserToken);
   }
 
   async create({
     user_id,
     refresh_token,
     expires_date,
-  }: ICreateUserTokenDTO): Promise<UserTokens> {
+  }: ICreateUserTokenDTO): Promise<UserToken> {
     const userToken = this.repository.create({
       user_id,
       refresh_token,
@@ -26,6 +26,22 @@ class UsersTokensRepository implements IUsersTokensRepository {
     await this.repository.save(userToken);
 
     return userToken;
+  }
+
+  async findByUserIdAndRefreshToken(
+    user_id: string,
+    refresh_token: string
+  ): Promise<UserToken> {
+    const userToken = await this.repository.findOne({
+      user_id,
+      refresh_token,
+    });
+
+    return userToken;
+  }
+
+  async deleteById(id: string): Promise<void> {
+    await this.repository.delete(id);
   }
 }
 
--- src/modules/accounts/repositories/IUsersTokensRepository.ts
+++ src/modules/accounts/repositories/IUsersTokensRepository.ts
@@ -1,12 +1,19 @@
 import { ICreateUserTokenDTO } from "../dtos/ICreateUserTokenDTO";
-import { UserTokens } from "../infra/typeorm/entities/UserTokens";
+import { UserToken } from "../infra/typeorm/entities/UserToken";
 
 interface IUsersTokensRepository {
   create({
     user_id,
     refresh_token,
     expires_date,
-  }: ICreateUserTokenDTO): Promise<UserTokens>;
+  }: ICreateUserTokenDTO): Promise<UserToken>;
+
+  findByUserIdAndRefreshToken(
+    user_id: string,
+    refresh_token: string
+  ): Promise<UserToken>;
+
+  deleteById(id: string): Promise<void>;
 }
 
 export { IUsersTokensRepository };
