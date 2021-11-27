745b5bbe4cd2e42465237b1a074b519d377ade7a
wladimirgrf <wladimir.code@gmail.com>
2021-11-27 16:41:28 -0300

    feat: auth config

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "e78a908f4370a9a140f4592eb787a8f8",
+  secret_refresh_token: "d41d8cd98f00b204e9800998ecf8427e",
+  expires_in_token: "15m",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
