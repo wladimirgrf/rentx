45c587d0f19b3e35eaa42099b7978cd2d92d29d4
wladimirgrf <wladimir.code@gmail.com>
2021-12-02 21:03:52 -0300

    feat: Reset password use case, controller and route

---
+++ src/modules/accounts/useCases/resetPassword/ResetPasswordUseCase.ts
@@ -0,0 +1,50 @@
+import { hash } from "bcryptjs";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { AppError } from "@shared/errors/AppError";
+
+interface IRequest {
+  token: string;
+  password: string;
+}
+
+@injectable()
+class ResetPasswordUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository,
+
+    @inject("UsersTokensRepository")
+    private usersTokensRepository: IUsersTokensRepository,
+
+    @inject("DateProvider")
+    private dateProvider: IDateProvider
+  ) {}
+
+  async execute({ token, password }: IRequest): Promise<void> {
+    const userToken = await this.usersTokensRepository.findByRefreshToken(
+      token
+    );
+
+    if (!userToken) {
+      throw new AppError("Token invalid", 403);
+    }
+
+    if (this.dateProvider.beforeNow(userToken.expires_date)) {
+      throw new AppError("Token expired", 401);
+    }
+
+    const user = await this.usersRepository.findById(userToken.user_id);
+
+    user.password = await hash(password, 8);
+
+    await this.usersRepository.create(user);
+
+    await this.usersTokensRepository.deleteById(userToken.id);
+  }
+}
+
+export { ResetPasswordUseCase };
--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
+import { ResetPasswordController } from "@modules/accounts/useCases/resetPassword/ResetPasswordController";
 import { SendForgotPasswordMailController } from "@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController";
 
 const passwordPublicRoutes = Router();
 
 const sendForgotPasswordMailController = new SendForgotPasswordMailController();
+const resetPasswordController = new ResetPasswordController();
 
 passwordPublicRoutes.post("/forgot", sendForgotPasswordMailController.handle);
+passwordPublicRoutes.post("/reset", resetPasswordController.handle);
 
 export { passwordPublicRoutes };
