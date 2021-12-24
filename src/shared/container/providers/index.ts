7884e52c6fe2113370b6d69f2eaafc75c56ff7fd
wladimirgrf <wladimir.code@gmail.com>
2021-12-23 21:04:47 -0300

    fix: providers index

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,26 +1,3 @@
-import { container } from "tsyringe";
-
-import { IDateProvider } from "./DateProvider/IDateProvider";
-import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvider";
-import { IMailProvider } from "./MailProvider/IMailProvider";
-import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
-import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
-import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
-import { IStorageProvider } from "./StorageProvider/IStorageProvider";
-
-container.registerSingleton<IDateProvider>("DateProvider", DayjsDateProvider);
-
-container.registerInstance<IMailProvider>(
-  "MailProvider",
-  new EtherealMailProvider()
-);
-
-const storageType = {
-  local: LocalStorageProvider,
-  s3: S3StorageProvider,
-};
-
-container.registerSingleton<IStorageProvider>(
-  "StorageProvider",
-  storageType[process.env.STORAGE_TYPE || "local"]
-);
+import "./DateProvider";
+import "./MailProvider";
+import "./StorageProvider";
