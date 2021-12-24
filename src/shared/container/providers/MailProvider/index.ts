2ad6413791e19ed53619ad218210289d0a0c7e07
wladimirgrf <wladimir.code@gmail.com>
2021-12-23 21:04:28 -0300

    feat: Mail provider index

---
+++ src/shared/container/providers/MailProvider/index.ts
@@ -0,0 +1,15 @@
+import { container } from "tsyringe";
+
+import { IMailProvider } from "./IMailProvider";
+import { EtherealMailProvider } from "./implementations/EtherealMailProvider";
+import { SESMailProvider } from "./implementations/SESMailProvider";
+
+const mailType = {
+  ethereal: container.resolve(EtherealMailProvider),
+  ses: container.resolve(SESMailProvider),
+};
+
+container.registerInstance<IMailProvider>(
+  "MailProvider",
+  mailType[process.env.MAIL_TYPE || "ethereal"]
+);
