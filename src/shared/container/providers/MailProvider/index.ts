99dff94d20ad1499b01babd77372fdd5ccd229c0
wladimirgrf <wladimir.code@gmail.com>
2022-01-12 20:39:28 -0300

    feat: MailProvider, add in-memory type

--- src/shared/container/providers/MailProvider/index.ts
+++ src/shared/container/providers/MailProvider/index.ts
@@ -3,13 +3,15 @@ import { container } from "tsyringe";
 import { IMailProvider } from "./IMailProvider";
 import { EtherealMailProvider } from "./implementations/EtherealMailProvider";
 import { SESMailProvider } from "./implementations/SESMailProvider";
+import { MailProviderInMemory } from "./in-memory/MailProviderInMemory";
 
 const mailType = {
   ethereal: container.resolve(EtherealMailProvider),
   ses: container.resolve(SESMailProvider),
+  test: container.resolve(MailProviderInMemory),
 };
 
 container.registerInstance<IMailProvider>(
   "MailProvider",
-  mailType[process.env.MAIL_TYPE || "ethereal"]
+  mailType[process.env.MAIL_TYPE]
 );
