1af79aa3d787f98d3cb4965a5f4bb5822bd90cc1
wladimirgrf <wladimir.code@gmail.com>
2022-01-12 11:57:15 -0300

    chore: typeorm create connection, format

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -3,12 +3,8 @@ import { createConnection, getConnectionOptions, Connection } from "typeorm";
 export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
-  return createConnection(
-    Object.assign(defaultOptions, {
-      database:
-        process.env.NODE_ENV === "test"
-          ? "rentx_test"
-          : defaultOptions.database,
-    })
-  );
+  const database =
+    process.env.NODE_ENV === "test" ? "rentx_test" : defaultOptions.database;
+
+  return createConnection(Object.assign(defaultOptions, { database }));
 };
