07b59bcceaf7a43be9a2ecf0f565edac40694786
wladimirgrf <wladimir.code@gmail.com>
2022-01-13 19:03:52 -0300

    fix: upload car images use case, invalid category id

--- src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
@@ -1,7 +1,9 @@
 import { inject, injectable } from "tsyringe";
 
 import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider";
+import { AppError } from "@shared/errors/AppError";
 
 interface IRequest {
   car_id: string;
@@ -14,11 +16,20 @@ class UploadCarImagesUseCase {
     @inject("CarsImagesRepository")
     private carsImagesRepository: ICarsImagesRepository,
 
+    @inject("CarsRepository")
+    private carsRepository: ICarsRepository,
+
     @inject("StorageProvider")
     private storageProvider: IStorageProvider
   ) {}
 
   async execute({ car_id, images_name }: IRequest): Promise<void> {
+    const carFound = await this.carsRepository.findById(car_id);
+
+    if (!carFound) {
+      throw new AppError("Car does not exists!");
+    }
+
     images_name.map(async (image_name) => {
       await this.carsImagesRepository.create(car_id, image_name);
       await this.storageProvider.save(image_name, "cars");
